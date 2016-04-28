7bf30644a611c1287680853634a5a5bcf5fcc498
Ihtasham <redrailz@gmail.com>
2016-04-28 02:01:01 +0100

    Update DbConfig.java

--- src/main/java/webAppProject/persistence/DbConfig.java
+++ src/main/java/webAppProject/persistence/DbConfig.java
@@ -13,10 +13,6 @@ public class DbConfig {
 		ds.setDriverClassName("com.mysql.jdbc.Driver");
 		// jdbc:mysql://host:port/db
 		
-		ds.setUrl("jdbc:mysql://mysql.mcscw3.le.ac.uk:3306/mic7");
-		ds.setUsername("mic7");
-		ds.setPassword("shigiiho");
-		return ds;
 		/*
 		ds.setUrl("jdbc:mysql://localhost:3306/webapp");
 		ds.setUsername("root");
