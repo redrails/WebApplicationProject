08ab8b3e30de5f87d5eeddec1422bf88c79d3744
redrails <me@ihtasham.com>
2016-04-28 02:29:12 +0100

    travis db connection

--- src/main/java/webAppProject/persistence/DbConfig.java
+++ src/main/java/webAppProject/persistence/DbConfig.java
@@ -12,7 +12,6 @@ public class DbConfig {
 		DriverManagerDataSource ds = new DriverManagerDataSource();
 		ds.setDriverClassName("com.mysql.jdbc.Driver");
 		// jdbc:mysql://host:port/db
-
 		ds.setUrl("jdbc:mysql://localhost:3306/webapp");
 		ds.setUsername("root");
 		ds.setPassword("");
